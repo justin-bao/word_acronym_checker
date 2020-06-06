db320cce0ea1c5b1434998870903497d0cb9b942
justin-bao <justin.bao91@gmail.com>
2020-06-05 17:28:02 -0700

    Initial commit

---
+++ word_acronym_checker.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+from word_table_reader import *
+from word_acronym_reader import *
+
+if __name__=="__main__":
+    import sys
+    
+    all_acronyms = get_all_acronyms(sys.argv[1])
+    explained_acronyms = get_explained_acronyms(sys.argv[1])
+
+    acronyms_in_table = []
+    acronyms_not_in_table = []
+
+    explained_acronyms_in_table = []
+    explained_acronyms_not_in_table = []
+
+    unexplained_acronyms = []
+
+    abbreviation_table = []
+
+    for table in get_docx_tables(sys.argv[1]):
+        row = get_text_for_table(table)[0]
+        if row[0].isupper():
+            abbreviation_table = []
+            break
+    
+    for row in get_text_for_table(abbreviation_table):
+        for acronym in all_acronyms:
+            if row[0] == acronym:
+                acronyms_in_table.append(acronym)
+            else:
+                acronyms_not_in_table.append(acronym)
+
+        for acronym in explained_acronyms:
+            if row[0] == acronym[0] and row[1] == acronym[1]:
+                explained_acronyms_in_table.append(acronym)
+            else:
+                explained_acronyms_not_in_table.append(acronym)
+
+    for acronym in explained_acronyms:
+        if acronym[0] not in all_acronyms:
+            unexplained_acronyms.append(acronym[0])
+
+
+    print("Acronyms not in the table: ")
+    for acronym in acronyms_not_in_table:
+        print(acronym)
+    print()
+
+    print("Acronyms that have not been explained: ")
+    for acronym in unexplained_acronyms:
+        print(acronym)
+    print()
+                
---
+++ word_acronym_reader.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+# Program for reading all the acronyms in a .docx file
+import re
+from docx import Document
+from docx.shared import Inches
+
+
+def get_all_acronyms(path):
+    """
+    Find every (possible) acronym in the document
+    (i.e. in the form of 'An Example Acronym (AEA)')
+    Looks for any alphanumeric text surrounded by parentheses
+    """
+
+    document = Document(path)
+    document.save(path)
+
+    acronyms = []
+    pattern = '[A-Z]\w*[A-Z]'
+
+    for para in document.paragraphs:
+        for acronym in re.findall(pattern, para.text):
+            if acronym not in acronyms:
+                acronyms.append(acronym)
+
+    return acronyms
+        
+
+def get_explained_acronyms(path):
+    """
+    Find every acronym that has been explained
+    (i.e. in the form of 'An Example Acronym (AEA)')
+    """
+
+    document = Document(path)
+    document.save(path)
+
+    acronymTuples = []
+    pattern = '\([A-Z]\w*[A-Z]\)'
+
+    for para in document.paragraphs:
+        for acronym in re.findall(pattern, para.text):
+            # backtrack until finding the number of words corresponding with the
+            # number of capital letters in the acronym
+            acronymIndex = para.text.index(acronym)
+            numOfWords = sum(1 for c in acronym if c.isupper())
+
+            
+            explanation = ""
+            acronymIndex -= 1
+
+            # go until reaching the start of the string or until all words are found
+            while acronymIndex >= 0 and numOfWords > 0:
+                if para.text[acronymIndex].isupper():
+                    numOfWords -= 1
+                explanation = para.text[acronymIndex] + explanation
+                acronymIndex -= 1
+
+            # remove parentheses from the acronym and leading/trailing whitespace
+            acronymTuples.append((acronym[1:-1], explanation.strip()))
+
+    return acronymTuples
+
+
+if __name__=="__main__":
+    import sys
+    print("get_all_acronyms:")
+    print(get_all_acronyms(sys.argv[1]))
+    print
+
+    print("get_explained_acronyms:")
+    print(get_explained_acronyms(sys.argv[1]))
+    print
